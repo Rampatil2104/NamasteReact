e1a15498ae1421f57e5c8262e66059e408ce001a
Ram Patil <84570020+Rampatil2104@users.noreply.github.com>
2024-01-06 04:43:16 -0800

    Add files via upload

---
+++ ep10/src/components/RestaurantCard.js
@@ -0,0 +1,22 @@
+import { CDN_URL } from "../utils/constant";
+const RestaurantCard = (props) =>{
+    console.log(props)
+    const{resData} = props;
+    const{cloudinaryImageId,name,cuisines,avgRating,costForTwo} = resData?.info;
+    return(
+            <div className="m-4 p-4 w-[220px] rounded-lg bg-gray-100 hover:bg-gray-500">
+              <img
+               className="rounded-lg" 
+               alt="res-logo" 
+               src={CDN_URL+cloudinaryImageId}
+               />
+              <h3 className="font-bold py-4 text-lg">{name}</h3>
+              <h4>{cuisines.join(", ")}</h4>
+              <h4>{avgRating}stars</h4>
+              <h4>{costForTwo}</h4>
+            </div>
+
+    );
+};
+
+export default RestaurantCard;
---
+++ ep10/src/components/Shimmer.js
@@ -0,0 +1,23 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+    <div className="animate-pulse flex space-x-4 bg-fuchsia-700"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    <div className="shimmer-card"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
